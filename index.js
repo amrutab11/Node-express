c46ec165cf95f0906d7690417de73fba9ff24cd0
amrutab11 <amrutabelgaonkar11998@gmail.com>
2020-10-11 21:03:43 +0530

    Assignment task 1 completed

--- index.js
+++ index.js
@@ -13,35 +13,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes',dishRouter);
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end("Will send details of the dsh: " +
-        req.params.dishId + " to you!!");
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation is not supported on /dishes/'+
-    req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write("Updating the dish: "+req.params.dishId+"\n");
-    res.end(" Will update the dish: "+ req.body.name+
-    " With details: "+ req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end("Deleting dish: "+req.params.dishId);
-});
-
-app.use(express.static(__dirname+'/public'));
-
-app.use((req, res, next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>This is an express server</h1></body></html>');
-});
-
 const server = http.createServer(app);
 
 server.listen(port,hostname,()=>{
